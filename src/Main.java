1751962abbc6c377fe30c5edee468ecfbe502860
Aswathy R <71879542+AswathyR005@users.noreply.github.com>
2021-06-24 23:44:25 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,25 @@
 public class Main {
 
     public static void main(String args[]) {
-        String question = "";
-        String choiceOne = "";
-        String choiceTwo = "";
-        String choiceThree = "";
+        String question = "who won test championship finals in 2021?";
+        String choiceOne = "India";
+        String choiceTwo = "NewZealand";
+        String choiceThree = "England";
 
         String correctAnswer = choiceTwo;
-
+        system.out.println(question);
+        system.out.println(choiceOne);
+        system.out.println(choiceTwo);
+        system.out.println(choiceThree);
+        Scanner input=new Scanner(system.in);
+        answer=input.next();
+        if(answer==choiceTwo){
+            system.out.println("Congrats!!");
+        }
+        else{
+            system.out.println("Wrong Answer");
+        }
+        
         // Write a print statement asking the question
         // Write a print statement giving the answer choices
 
